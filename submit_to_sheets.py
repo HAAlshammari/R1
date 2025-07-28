7707263fefbedb641f8fadc8499a64e8d495a842
HAAlshammari <41627640+HAAlshammari@users.noreply.github.com>
2025-07-28 10:26:59 +0300

    submit_to_sheets.py

--- submit_to_sheets.py
+++ submit_to_sheets.py
@@ -2,20 +2,20 @@ import gspread
 from google.oauth2 import service_account
 import streamlit as st
 
-def submit_to_sheet(date, flat_a, flat_b, flat_c, total_cost, total_income, investor_shares):
+def submit_to_sheet(date, flats_a, flats_b, flats_c, total_cost, total_income, investor1_income, investor2_income, investor3_income):
     creds_dict = st.secrets["gcp_service_account"]
     creds = service_account.Credentials.from_service_account_info(dict(creds_dict))
     client = gspread.authorize(creds)
 
     sheet = client.open("RentalData").sheet1  
     sheet.append_row([
-        date,
-        flat_a,
-        flat_b,
-        flat_c,
-        total_cost,
-        total_income,
-        investor_shares[0],
-        investor_shares[1],
-        investor_shares[2]
+    date,
+    flats_a,
+    flats_b,
+    flats_c,
+    total_cost,
+    total_income,
+    investor1_income,
+    investor2_income,
+    investor3_income
     ])
