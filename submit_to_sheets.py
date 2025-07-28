0a62338f13d5a6ceb7f987a95046722524ea1ce2
HAAlshammari <41627640+HAAlshammari@users.noreply.github.com>
2025-07-28 10:38:55 +0300

    submit_to_sheets.py

--- submit_to_sheets.py
+++ submit_to_sheets.py
@@ -1,21 +1,30 @@
-import gspread
-from google.oauth2 import service_account
 import streamlit as st
+import gspread
+from google.oauth2.service_account import Credentials
 
-def submit_to_sheet(date, flats_a, flats_b, flats_c, total_cost, total_income, investor1_income, investor2_income, investor3_income):
-    creds_dict = st.secrets["gcp_service_account"]
-    creds = service_account.Credentials.from_service_account_info(dict(creds_dict))
-    client = gspread.authorize(creds)
+# Define scope for Sheets API
+SCOPES = ["https://www.googleapis.com/auth/spreadsheets"]
 
-    sheet = client.open("RentalData").sheet1  
-    sheet.append_row([
-    date,
-    flats_a,
-    flats_b,
-    flats_c,
-    total_cost,
-    total_income,
-    investor1_income,
-    investor2_income,
-    investor3_income
-    ])
+# Load credentials from Streamlit Secrets
+service_account_info = {
+    "type": st.secrets["gcp_service_account"]["type"],
+    "project_id": st.secrets["gcp_service_account"]["project_id"],
+    "private_key_id": st.secrets["gcp_service_account"]["private_key_id"],
+    "private_key": st.secrets["gcp_service_account"]["private_key"],
+    "client_email": st.secrets["gcp_service_account"]["client_email"],
+    "client_id": st.secrets["gcp_service_account"]["client_id"],
+    "auth_uri": st.secrets["gcp_service_account"]["auth_uri"],
+    "token_uri": st.secrets["gcp_service_account"]["token_uri"],
+    "auth_provider_x509_cert_url": st.secrets["gcp_service_account"]["auth_provider_x509_cert_url"],
+    "client_x509_cert_url": st.secrets["gcp_service_account"]["client_x509_cert_url"],
+}
+
+credentials = Credentials.from_service_account_info(service_account_info, scopes=SCOPES)
+client = gspread.authorize(credentials)
+
+# You can change "RentalData" to your actual Google Sheet name
+sheet = client.open("RentalData").sheet1
+
+def submit_to_sheet(date, flats_a, flats_b, flats_c, total_cost, total_income, investor1, investor2, investor3):
+    data = [str(date), flats_a, flats_b, flats_c, total_cost, total_income, investor1, investor2, investor3]
+    sheet.append_row(data)
